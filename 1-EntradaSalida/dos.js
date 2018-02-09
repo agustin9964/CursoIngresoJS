1023c9db636858d64f7b474fa849989c6a466985
agustin9964 <36265053+agustin9964@users.noreply.github.com>
2018-02-09 12:29:56 -0300

    Clase 2

---
+++ 1-EntradaSalida/dos.js
@@ -0,0 +1,15 @@
+function Mostrar()
+{
+    var producto;
+    var iva;
+    
+    producto= prompt("importe de iva");
+    producto= parseInt(producto);
+
+    iva= producto*1,21;
+
+    document.getElementById()
+
+
+  
+}
--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,8 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
+    var importe
+    var RESULTADO
+    var
 	
 }
