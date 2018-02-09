9e6ef6e32186e857a6c64b9193240f39d5313f07
agustin9964 <36265053+agustin9964@users.noreply.github.com>
2018-02-09 10:28:56 -0300

    Ejercicios 1 al 6 curso de ingreso

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,22 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function Mostar()
 {	
-	
+    var nombre;
+    var edad;
+
+    nombre=document.getElementById("elNombre").value;
+    edad=document.getElementById("laEdad").value
+    
+    alert( "Su nombre es " + nombre + " y usted tiene "+ edad + " años ");
+    
+    //alert(nombre)
+
+
+
+    //alert(edad)
+
+
+
+
 }
 
