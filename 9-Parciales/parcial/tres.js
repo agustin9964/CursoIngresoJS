af5c78703de0db663020579305687e1ca04f9082
agustin9964 <36265053+agustin9964@users.noreply.github.com>
2018-02-10 22:33:45 -0300

    Parcial programación
    perimetros, largo, ancho, iva

--- 9-Parciales/parcial/tres.js
+++ 9-Parciales/parcial/tres.js
@@ -1,4 +1,36 @@
 function Mostrar()
 {
+	
+    //se identifican las variables;
+
+    // se les da valor por ID;
+
+    // se las hace enteros con parseInt;
+
+    // se le da valor al perimetro del rectangulo;
+
+    // se da el resultado por alert ;
+
+
+	var largo;
+	var ancho;
+	var perimetro;
+
+
+	largo=document.getElementById("alrgo").value;
+	ancho=document.getElementById("ancho").value;
+
+	largo=parseInt(largo);
+	ancho=parseInt(ancho);
+
+
+	perimetro=largo*2+ancho*2;
+
+
+	alert(" se necesitan "+ perimetro*3 +" metros de alambrado para colocarle 3 hilos a su perimetro");
+
+
+
+
 
 }
--- 9-Parciales/parcial/uno.js
+++ 9-Parciales/parcial/uno.js
@@ -1,5 +1,23 @@
 
 function Mostrar()
 {
+	// se identifican las variables;
+
+	//se le da valor por Id;
+
+	//se le da valor al perimetro del cuadrado;
+
+	// se muestra por alert el valor del perimetro;
+
+
+
+	var base;
+	var perimetro;
+
+	base= document.getElementById('laBase').value;
+
+	perimetro=base*4;
+
+	alert(" el perimetro es " + perimetro);
 
 }
