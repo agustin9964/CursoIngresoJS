5aafeafe5cc1133a47a86d188a4d1dd03b2cdbd5
agustin9964 <36265053+agustin9964@users.noreply.github.com>
2018-02-15 12:20:35 -0300

    Ejercicio parcial
    else,if y document.write

--- 9-Parciales/parcial/cuatro.js
+++ 9-Parciales/parcial/cuatro.js
@@ -1,4 +1,35 @@
 function Mostrar()
 {
+    var numeroUno;
+    var numeroDos;
+    var resultado;
 
+    numeroUno=prompt("ponga su primer numero");
+
+    numeroDos=prompt("ponga su segundo numero");
+
+    numeroUno=parseInt(numeroUno);
+    numeroDos=parseInt(numeroDos);
+
+    if(numeroUno==numeroDos)
+    { 
+        resultado=numeroUno*numeroDos;
+        document.write(resultado);
+
+    }
+    else
+    { 
+        if(numeroUno>numeroDos)
+        { 
+            resultado=numeroUno-numeroDos;
+            document.write(resultado);
+
+    }
+     else
+     {
+         resultado=numeroUno+numeroDos;
+         document.write(resultado);
+     } 
+
+    }
 }
