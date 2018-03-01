c73cfba2a52458849c98cd90462bb2f9736b67d9
agustin9964 <36265053+agustin9964@users.noreply.github.com>
2018-03-01 12:24:25 -0300

    for ejercicios 7 y 8

--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -1,5 +1,19 @@
 function Mostrar()
 {
+    var numero=prompt("Ingrese un numero");
+    var contador=0;
+
+    for(i=1;i<numero;i++)
+    {
+        if(numero%i==0)
+        {         
+            contador++;
+            console.log(i);
+        }
+    }
+
+   
+
 
 
 
--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,6 +1,27 @@
 function Mostrar()
 {
+    var numero=prompt("Ingrese un numero");
+    var contador=0;
 
+    for(i=2;i<numero;i++)
+        {
+            if(numero%i==0)
+            {
+                contador=3;
+                break;
+            
+
+            }
+        }
+    if  (contador==2)
+    {
+        alert("es primo");
+
+    }
+    else 
+    {
+        alert("no es primo");
+    }
 
 
 
