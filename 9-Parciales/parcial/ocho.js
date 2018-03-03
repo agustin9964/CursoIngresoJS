5fc188a181e58700b46ba5e2fa7b889a5deb6dcb
agustin9964 <36265053+agustin9964@users.noreply.github.com>
2018-03-03 00:45:48 -0300

    ejercicio 8 octavio y pagina

--- 9-Parciales/parcial/ocho.js
+++ 9-Parciales/parcial/ocho.js
@@ -8,49 +8,141 @@ function Mostrar()
     // la cantidad de temperaturas pares e impares
     // la cantidad de temperaturas positivas y negativas
     // el sexo del animal cuyo habitat tiene como temperatura 0
-    var nombreAnimal;
+    /*var nombreAnimal;
     var peso;
     var temperatura;
     var sexo;
-    var habitatDelAnimal;
     var respuesta="si";
     var contador=0;
+    var temperaturaMaxima;
+    var pesoMaximo;
+    var animalMasPesado;
+    var animalConElHabitatMasFrio;
+    var sexoTemperatura;
+    var temperaturaPar=0;
+    var temperaturaImpar=0;
+    var temperaturaPositiva;
+    var temperaturaNegativa;
 
     while(respuesta!="no")
     {
         contador++;
         nombreAnimal=prompt(" Ingrese el nombre de un animal");
-        if (nombreAnimal==null)
-        {
-            break;
-        }
-    else{
-            peso=prompt(" Ingrese el peso de su animal");
-
-            if(peso==null)
-                {
-                    break;
-                }
-            else if()        
+        while(isNaN(nombreAnimal)||nombreAnimal=="")
+            {
+                 nombreAnimal=prompt(" Ingrese el nombre de un animal");
 
             }
-            temperatura=prompt(" Ingrese la temperatura");
-            sexo=prompt("ingrese el sexo");
-            habitatDelAnimal=prompt("ingrese el habitat del animal");
+        peso=prompt(" Ingrese el peso de su animal");
+        peso=parseInt(peso);
+        while(peso<=0||isNaN(peso))    
+            {
+                peso=prompt(" Ingrese el peso de su animal");
+                peso=parseInt(peso);
+
+            }
+
+        sexo=prompt(" Ingrese el sexo de su animal, presione f si es femenino o m si es masculino");
+
+        while(sexo!="f"||sexo!="m")
+            {
+                sexo=prompt(" Ingrese el sexo de su animal, presione f si es femenino o m si es masculino");
+
+            }
+
+
+        temperatura=prompt(" Ingrese la temperatura del habitat"); 
+        temperatura=parseInt(temperatura);
+        while(temperatura<-40||temperatura>40)
+            {
+                temperatura=prompt(" Ingrese la temperatura del habitat"); 
+                temperatura=parseInt(temperatura);
+
+            }
 
 
 
 
-    }
 
 
-    }
+        respuesta=prompt("¿Desea continuar ingresando animales?");            
+    }//while(respuesta!="no")
 
+
+}*/
+
+    var numero;
+    var contador=0;
+    var cantidadNumerosPares=0;
+    var respuesta="si";
+    var maximo;
+    var minimo;
+    var sumaNumeros=0;
+    var promedioNumeros;
+
+
+    while(respuesta!="no")
+        {
+            contador++;
+
+            numero=prompt("Ingrese un numero");
+
+            numero=parseInt(numero);
+
+            
+
+            while(numero<=0|| isNaN(numero))
+                {
+                    numero=prompt(" Ingrese un numero mayor a 0");
+                    numero=parseInt(numero);
+
+                }
+            sumaNumeros=sumaNumeros+numero;
+
+            if(numero%2==0&&numero!=0)
+                {
+                    cantidadNumerosPares++;
+
+                }
+            
+            if(contador==1)
+                {
+                         maximo=numero;
+                         minimo=numero;
+                }
+             else
+                {
+                     if(numero>maximo)
+                        {
+                              maximo=numero;
+
+                        }
+                    else
+                        {
+                            minimo=numero;
+                         }    
+                } 
+
+        respuesta=prompt("Si quiere salir, ingresar no");
+
+        } ////while(respuesta!="no") 
+
+
+
+
+                   
+        
+
+    sumaNumeros=sumaNumeros+numero;
 
     
-    
-    
-   
+   promedioNumeros=sumaNumeros/contador;
+
+   document.write(" la suma de todos los numeros es de " + sumaNumeros + "<br>");
+   document.write(" el numero maximo es " + maximo+ "<br>");
+   document.write(" el numero minimo es " + minimo+ "<br>");
+   document.write(" el promedio de todos los numeros es " + promedioNumeros + "<br>");
+   document.write(" la cantidad de numeros pares es " + cantidadNumerosPares + "<br>");
     
 
    
